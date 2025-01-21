96382e69d77a277f2d855b484c0aa8a58d758a33
Joao Pedro Milhome <pedro.milhome@hotmail.com>
2025-01-21 16:18:31 -0300

    feat: ajuste no dns

--- records.tf
+++ records.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_record" "any_record_a" {
   zone_id = var.cloudflare_zone_id
   name    = "demo1.jcloud.com.br"
-  content = "198.51.100.45"
+  content = "198.51.100.15"
   type    = "A"
   proxied = true
 }
