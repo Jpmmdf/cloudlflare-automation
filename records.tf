1eb827a55565f655877aeb4ea5fd95d0742653e2
Joao Pedro Milhome <pedro.milhome@hotmail.com>
2025-01-21 16:02:01 -0300

    feat: ajustando a pipeline

--- records.tf
+++ records.tf
@@ -1,7 +1,7 @@
 resource "cloudflare_record" "any_record_a" {
   zone_id = cloudflare_zone.domain_zone.id
   name    = "demo1.jcloud.com"
-  content = "198.51.100.4"
+  content = "198.51.100.45"
   type    = "A"
   proxied = true
 }
