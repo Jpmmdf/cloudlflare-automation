21f6c872e2a39e75e2eadafd23305f36d8a485e6
Joao Pedro Milhome <pedro.milhome@hotmail.com>
2025-01-21 09:33:52 -0300

    feat: primeiro commit

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  cloud {
+    organization = "JPITCORP"  
+
+    workspaces {
+      name = "cloudlflare-automation"
+    }
+  }
+}
---
+++ records.tf
@@ -0,0 +1,14 @@
+resource "cloudflare_dns_record" "example_dns_record" {
+  zone_id = cloudflare_zone.domain_zone.id
+  comment = "Domain verification record"
+  content = "198.51.100.4"
+  name = "demo.jcloud.com"
+  proxied = true
+  settings = {
+    ipv4_only = true
+    ipv6_only = true
+  }
+  tags = ["owner:dns-team"]
+  ttl = 3600
+  type = "A"
+}
