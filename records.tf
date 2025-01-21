8b9195fc329d73c1fe6c102a2bf60a54586e05b0
Joao Pedro Milhome <pedro.milhome@hotmail.com>
2025-01-21 09:50:31 -0300

    feat: alterando o dns name

--- records.tf
+++ records.tf
@@ -2,7 +2,7 @@ resource "cloudflare_dns_record" "example_dns_record" {
   zone_id = cloudflare_zone.domain_zone.id
   comment = "Domain verification record"
   content = "198.51.100.4"
-  name = "demo.jcloud.com"
+  name = "demo1.jcloud.com"
   proxied = true
   settings = {
     ipv4_only = true
